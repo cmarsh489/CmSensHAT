8e92d95061b6c43bebc902f9234ac8fb256bcdce
cmarsh489 <cmarsh489@lsr7.net>
2018-12-11 13:25:57 -0600

    created a copy of treasure game to attempt to make it more difficult, added a file to build semester project in

--- Treasure_gamenew.py
+++ Treasure_gamenew.py
@@ -1,7 +1,7 @@
 #But better
 
 #!/bin/python3
-
+from sense_hat import SenseHat
 from sense_hat import *
 from time import sleep
 from random import randint
@@ -23,7 +23,7 @@ W = [255, 255, 255] # white
 
 score=0
 
-for turns in range(10):
+for turns in range(5):
   
   
   coinx=randint(0,7)
@@ -47,6 +47,50 @@ for turns in range(10):
         score+= 1
       else:
         sense.set_pixel(x,y,R)
+        
+      sleep(1)
+      sense.clear()
+      break;
+    sense.clear()
+    if e.direction== DIRECTION_UP and y>0:
+      y= y-1
+    elif e.direction == DIRECTION_DOWN and y<7:
+      y=y+1
+    elif e.direction == DIRECTION_LEFT and x>0:
+      x= x - 1
+    elif e.direction == DIRECTION_RIGHT and x<7:
+      x=x+1
+    sense.set_pixel(x,y,W)
+        
+for turns in range(5):
+  coin1x=randint(0,7)
+  coin1y=randint(0,7)
+  
+  coin2x=randint(0,7)
+  coin2y=randint(0,7)
+  print(coinx, coiny)
+  
+  sense.set_pixel(coin1x, coin1y, Y)
+  sense.setpixel(coin2x, coin2y, Y)
+  
+  sleep(2)
+  sense.clear()
+  
+  x=randint(0,7)
+  y=randint(0,7)
+  sense.set_pixel(x, y, W)
+  
+  while True:
+    e = wait_for_move()
+    if e.direction == DIRECTION_MIDDLE:
+      
+      if x == coin1x and y== coin1y:
+        sense.set_pixel(x,y,G)
+        score+= 1
+      else:
+        sense.set_pixel(x,y,R)
+
+    
       
       sleep(1)
       sense.clear()
@@ -64,4 +108,6 @@ for turns in range(10):
     sense.set_pixel(x,y,W)
 sense.show_message("Score: " + str(score))
 print (score)
+
+
 
