ac5cd094e34deff6613c5a507bb165d48fbbb650
cmarsh489 <45769834+cmarsh489@users.noreply.github.com>
2018-12-17 00:10:20 -0600

    got into position to "finish"
    still need to fix the wait_for_move() bug

--- semsester_project_game.py
+++ semsester_project_game.py
@@ -42,6 +42,27 @@ sword= [b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,w,b,
         b,b,b,b,b,b,b,b,
         ] #The player's "sprite"
+def s_up():
+  s_direction=1
+  sx1=6
+  sy1=3
+  sx2=6
+  sy2=4
+  sx3=6
+  sy3=5
+  sx4=6
+  sy4=6
+  sx5=5
+  sy5=5
+  sx6=7
+  sy6=5
+  sense.set_pixel(sx1,sy1,w)
+  sense.set_pixel(sx2,sy2,w)
+  sense.set_pixel(sx3,sy3,w)
+  sense.set_pixel(sx4,sy4,w)
+  sense.set_pixel(sx5,sy5,w)
+  sense.set_pixel(sx6,sy6,w)
+  
 
 arrowd= [(2,1,w),(2,2,w),(2,3,r)] 
 
@@ -69,7 +90,10 @@ x=1
 y1=0
 y2=1
 y3=2
+lives=3
 
+
+  
 
 
 """My idea with this is to make a game that has different direction arrows that come at you, and you have to match the direction to "beat" the arrow, much like that tower game I used to play.
@@ -87,13 +111,7 @@ for turns in range(3):
   a_direction= randint(1,4)
   def move_down(time):
     while True:
-      sleep(time)
-      y1-=1
-      y2-=1
-      y3-=1
-      sense.set_pixel(x1,y1,w)
-      sense.set_pixel(x2,y2,w)
-      sense.set_pixel(x3,y3,r)
+     pass
 
     
       
@@ -129,7 +147,7 @@ for turns in range(3):
   print a_direction
   s_direction=0
   score=0
-  lives=3
+  
   
 
     
@@ -144,30 +162,50 @@ for turns in range(3):
   sense.set_pixel(x3,y3,r)
   
 
-  while y2>6:
+  while True:
+    sense.clear()
+    s_up()
     print "hi"
-    e = wait_for_move()
-    sleep(1)
+    
+    
+    
     y1+=1
     y2+=1
     y3+=1
     sense.set_pixel(x1,y1,w)
     sense.set_pixel(x2,y2,w)
     sense.set_pixel(x3,y3,r)
-    print y1
-    print y2
-    print y3
+    sleep(2)
     
     
-    if e.direction == DIRECTION_MIDDLE and s_direction == a_direction and y2==6:
-      score+=1
-      sense.clear()
+    
+    if x1==12:
+      e = wait_for_move()
+      if e.direction== DIRECTION_UP and y>0:
+        s_up()
+      elif e.direction == DIRECTION_DOWN and y<7:
+        y=y+1
+      elif e.direction == DIRECTION_LEFT and x>0:
+        x= x - 1
+      elif e.direction == DIRECTION_RIGHT and x<7:
+        pass
+      if e.direction == DIRECTION_MIDDLE and s_direction == a_direction and y2==6:
+        score+=1
+        sense.clear()
+        break
+      elif y2!=6 and e.direction == DIRECTION_MIDDLE:
+        lives -= 1
+        sense.show_message("Lives = " +str(lives), scroll_speed= .01, text_colour=r)
       
-    elif y2!=6 and e.direction == DIRECTION_MIDDLE:
-      lives -= 1
-      sense.show_message("Lives = " +str(lives), scroll_speed= .03, text_colour=r)
-      sense.clear()
-    
+        sense.clear()
+        
+      else:
+        sense.clear()
+    if y2 ==6:
+        lives -= 1
+        sense.show_message("Lives = " +str(lives), scroll_speed= .01, text_colour=r)
+        break;
+        
       
     
       
